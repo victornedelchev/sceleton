e0d56f24b2735bc9aa7b463afd5f2547c6498562
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2024-02-09 21:00:43 +0200

    Add images and css in public directory

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
-const app = express();
+const path = require('path');
 const { PORT } = require('./constants');
+const routes = require('./router');
 
+// Init
+const app = express();
+
+// Express configurations
+app.use(express.static(path.resolve(__dirname, 'public')));
 app.use(express.urlencoded({ extended: false }));
 
+// Routes
 app.get('/', (req, res) => {
     res.send('OK!');
 });
+
+app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is listening on http://localhost:${PORT}...`));
