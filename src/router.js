59f2f847bded5db479b8484639e44e82375b04d8
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2024-02-09 23:30:30 +0200

    Prepare user functionality

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const homeController = require('../src/Controllers/homeController');
+const userController = require('../src/Controllers/userController');
 
 router.use(homeController);
+router.use('/users', userController);
 
 module.exports = router;
