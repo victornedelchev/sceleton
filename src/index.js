0352d72ea774e07899ab68ee7e95b5f7c4d9a55e
Victor Nedelchev <112422875+victornedelchev@users.noreply.github.com>
2024-02-09 20:28:42 +0200

    Config body parser

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const { PORT } = require('./constants');
 
+app.use(express.urlencoded({ extended: false }));
+
 app.get('/', (req, res) => {
     res.send('OK!');
 });
